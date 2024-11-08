17b0c43ac26930ccdf4f7c99d242ff340050e2a8
⌯ 𝐙ᴇᴡsᴛᴀ ×͜× <Zewsta0@gmail.com>
2024-11-08 23:22:14 +0300

    Update ping.py

--- WinxMusic/plugins/tools/ping.py
+++ WinxMusic/plugins/tools/ping.py
@@ -7,7 +7,6 @@ from WinxMusic import app
 from WinxMusic.core.call import Winx
 from WinxMusic.utils import bot_sys_stats
 from WinxMusic.utils.decorators.language import language
-from WinxMusic.utils.inline import support_group_markup
 from config import BANNED_USERS, PING_IMG_URL
 from strings import get_command
 
@@ -17,9 +16,8 @@ PING_COMMAND = get_command("PING_COMMAND")
 @app.on_message(filters.command(PING_COMMAND) & ~BANNED_USERS)
 @language
 async def ping_com(_client: Client, message: Message, _):
-    response = await message.reply_photo(
-        photo=PING_IMG_URL,
-        caption=_["ping_1"].format(app.mention),
+    response = await message.reply_text(
+        text=_["ping_1"].format(app.mention),
     )
     start = datetime.now()
     pytgping = await Winx.ping()
@@ -35,5 +33,4 @@ async def ping_com(_client: Client, message: Message, _):
             DISK,
             pytgping,
         ),
-        reply_markup=support_group_markup(_),
     )
